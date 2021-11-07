1447a72f6342d72fe6f2162a0d1253ed9a0bea0c
chiefsmurph <chiefsmurph@gmail.com>
2021-11-06 21:14:12 -0700

    lat 100

--- example/group-call/browser/presenter.js
+++ example/group-call/browser/presenter.js
@@ -54,7 +54,7 @@ sf.model('presenter', function(My){
 		}	
 
 		// Set latency to 100ms (Equal with streamer)
-		const latency = 0;
+		const latency = 100;
 		presenterInstance = new ScarletsMediaPresenter({
 			mimeType:'audio/webm;codecs=opus', // Optional
 			element, // Optional
--- example/group-call/browser/streamer.js
+++ example/group-call/browser/streamer.js
@@ -28,7 +28,7 @@ sf.model('streamer', function(My){
 		if(My.listening[presenterID] === undefined){
 			// Set latency to 100ms (Equal with presenter)
 			var streamer = {
-				instance:new ScarletsAudioStreamer(0),
+				instance:new ScarletsAudioStreamer(100),
 				recvBytes:0,
 				bufferHeader:false
 			};
